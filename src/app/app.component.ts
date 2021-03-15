fda62b583efad365b2feba6cbaa17a500e23faef
Robert Santana <robertsantana@deliveron.com>
2021-03-15 19:38:38 -0300

    min repro steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pwa-appinsights-tests';
+
+  ngOnInit(): void {
+    setTimeout(() => console.log('Greetings from Component'), 30000);
+  }
 }
